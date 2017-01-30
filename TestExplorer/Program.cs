a64662ff2748e4e2824f374c6a4d1f57ce33d0e4
Aleksandr Strihin <aistrihin@gmail.com>
2017-01-30 17:50:02 +0200

    first sprint. 30.01

---
+++ TestExplorer/Program.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExplorer
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {  
+        }
+        [SetUp]
+        public void Initialize()
+        {
+           
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine("Opened URL");
+        }
+         [Test]
+        public void ExecuteTest()
+        {           
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("execute");
+            Console.WriteLine("Executed test");
+        }
+        [Test]
+        public void NextStep()
+        {
+            Console.WriteLine("Next method..");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Close the browser");
+        }
+    }
+}
